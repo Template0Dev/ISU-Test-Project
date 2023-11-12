e23e5f7b863f5c3a1b630f490edfbd47bf203877
Template Dev <148116566+Template0Dev@users.noreply.github.com>
2023-11-12 18:26:32 +0500

    Report views have been created.

--- src/resources/views/overview/Specific.blade.php
+++ src/resources/views/overview/Specific.blade.php
@@ -1,13 +1,59 @@
 <!DOCTYPE html>
 
 <html>
+
     <head>
-    </head>
+        <meta charset="utf-8" />
+        <title>Обзор</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}" />
+        <link rel="stylesheet" href="{{ asset('resources/css/table.css') }}" />
+        <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css" />
 
     <body>
-        <?php
-            $val = $_GET['faculty'];
-            echo "<p>$val</p>";
-        ?>
+        <div class="center pa3 sans-serif parent">
+            <h1 class="mb4" style="text-align: center">
+                Факультет{{ count($faculties) > 1 ? 'ы' : '' }}: <br />
+                {{ implode(', ', $faculties) }}.
+            </h1>
+            <div class="table-container">
+                <h2 style="text-align: center">Всего записей: {{ count($model) }}.</h2>
+                <table class="table-content">
+                    <thead>
+                        <tr>
+                            <th>Факультет / Институт</th>
+
+                            <th>Форма обучения</th>
+                            <th>Группа</th>
+                            <th>Бюджет?</th>
+
+                            <th>ISU ID</th>
+                            <th>Фамилия</th>
+                            <th>Имя</th>
+                            <th>Отчество</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($model as $data)
+                            <tr>
+                                <td>{{ $data->faculty_name }}</td>
+
+                                <td>{{ $data->education_form }}</td>
+                                <td>{{ $data->student_group }}</td>
+                                <td>{{ $data->on_contract ? 'Нет' : 'Да' }}</td>
+
+                                <td>{{ $data->student_isu_id }}</td>
+                                <td>{{ $data->student_surname }}</td>
+                                <td>{{ $data->student_name }}</td>
+                                <td>{{ $data->student_patronymic ?? '———' }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </body>
+
 </html>
--- src/resources/views/overview/Summary.blade.php
+++ src/resources/views/overview/Summary.blade.php
@@ -1,10 +1,84 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-    </head>
 
+    <head>
+        <meta charset="utf-8" />
+        <title>Обзор</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}" />
+        <link rel="stylesheet" href="{{ asset('resources/css/table.css') }}" />
+        <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css" />
+
+        <style>
+            .table-container {
+                padding: 5px;
+
+                border: 2.5px solid black;
+                border-collapse: collapse;
+                border-radius: 8px;
+            }
+
+            .table-content tbody tr:last-child {
+                background-color: gray;
+            }
+        </style>
     <body>
-        TD.
+        <div class="center pa3 sans-serif parent">
+            <h1 class="mb4" style="text-align: center">Полная отчётность.</h1>
+            <h2 style="text-align: center">Всего доступно: {{ count($model) }}.</h2>
+
+            <div class="table-container">
+                <table class="table-content">
+                    <thead>
+                        <tr>
+                            <th>Факультет / Институт</th>
+
+                            <th>Очное (Всего)</th>
+                            <th>Очное (Бюджет)</th>
+                            <th>Очное (Договор)</th>
+
+                            <th>Очно-Заочное (Всего)</th>
+                            <th>Очно-Заочное (Бюджет)</th>
+                            <th>Очно-Заочное (Договор)</th>
+
+                            <th>Заочное (Всего)</th>
+                            <th>Заочное (Бюджет)</th>
+                            <th>Заочное (Договор)</th>
+
+                            <th>Всего</th>
+                            <th>Бюджет</th>
+                            <th>Договор</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($model as $data)
+                            <tr>
+                                <td>{{ $data->{'Факультет / Институт'} }}</td>
+
+                                <td>{{ $data->{'Очное (Всего)'} }}</td>
+                                <td>{{ $data->{'Очное (Бюджет)'} }}</td>
+                                <td>{{ $data->{'Очное (Договор)'} }}</td>
+
+                                <td>{{ $data->{'Очно-Заочное (Всего)'} }}</td>
+                                <td>{{ $data->{'Очно-Заочное (Бюджет)'} }}</td>
+                                <td>{{ $data->{'Очно-Заочное (Договор)'} }}</td>
+
+                                <td>{{ $data->{'Заочное (Всего)'} }}</td>
+                                <td>{{ $data->{'Заочное (Бюджет)'} }}</td>
+                                <td>{{ $data->{'Заочное (Договор)'} }}</td>
+
+                                <td>{{ $data->{'Всего'} }}</td>
+                                <td>{{ $data->{'Бюджет'} }}</td>
+                                <td>{{ $data->{'Договор'} }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </body>
+
 </html>
